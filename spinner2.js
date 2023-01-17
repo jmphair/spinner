3b3acf004eb996adb5bd4b637de48a79431c287c
Jonathan Phair <jmphair@gmail.com>
2023-01-17 15:25:31 -0500

    needed some help to get it out of an infinite loop but got there thanks to some peers

--- spinner2.js
+++ spinner2.js
@@ -1,42 +1,22 @@
-// process.stdout.write('hello from spinner1.js... \rheyyy\n')
+// there was a delay of 200ms between each in the orginal so capture that delay in a variable
+let frameDelay = 200;
+// we can use an array to hold all the animation frames that we'll need
+const frames = ['|', '/', '-', '\\'];
+// to get the timeout that we'll need for how long the animation runs for
+const timeout = frameDelay * frames.length;
 
+// we can use a function to run each step of our code
+const frameLoop = setInterval(() => {
+  // loop over the frames and print them to the console use \r to keep them in the same spot
+  for (let i = 0; i < frames.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(`\r ${frames[i]}   `); // the frame is written in place thanks to \r
+    }, frameDelay * i); // the frame delay is multiplied by the index to get the next delay value
+  }
+}, timeout);
+// we need the below to both create a new line so we don't get the same bug as typewriter and also to
+// run clearInterval so that we don't have an infinite loop! thanks peers haha
 setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   '); 
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   '); 
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   '); 
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   '); 
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|  '); 
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write("\n");
-}, 1700);
+  clearInterval(frameLoop);
+  process.stdout.write('\n');
+}, 4000); 
